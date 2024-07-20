8577f8c9369ee27cc3169d8b4003e5709b958a7c
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-07-20 12:12:20 +0500

    first commit

---
+++ empty.php
@@ -0,0 +1,24 @@
+<?php
+ 
+ $name = "HANZLA";
+
+ $age = null ;
+
+if(empty($name)){
+
+    echo "This is empty";
+}
+else {
+    echo "Not empty";
+}
+
+
+if (empty($age)){
+    echo "This is empty";
+}
+else{
+    echo "Not empty";
+}
+
+
+?>
